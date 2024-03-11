11f146514ecdfc05703469e0e0fe41662320d546
Rami Jebara <rami.jebara@hyperviewhq.com>
2024-03-11 00:18:44 -0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+// Single threaded The One Billion Row Challenge in rust
+// More info: https://github.com/gunnarmorling/1brc
+use color_eyre::eyre::Result;
+use log::info;
+use std::{
+    collections::HashMap,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+fn main() -> Result<()> {
+    env_logger::init();
+    let file = File::open("data/measurements.txt")?;
+    let mut data_lines = BufReader::new(file).lines();
+    let mut cities: HashMap<String, WeatherSummary> = HashMap::new();
+
+    while let Some(Ok(line)) = data_lines.next() {
+        if let Some((city, measurement)) = line.split_once(';') {
+            if let Ok(value) = measurement.parse::<f64>() {
+                cities
+                    .entry(city.to_string())
+                    .and_modify(|city_stats| {
+                        city_stats.min = value.min(city_stats.min);
+                        city_stats.max = value.max(city_stats.max);
+                        city_stats.sum += value;
+                        city_stats.reading_count += 1.0;
+                    })
+                    .or_insert(WeatherSummary {
+                        min: value,
+                        max: value,
+                        sum: value,
+                        reading_count: 1.0,
+                    });
+            }
+        }
+    }
+
+    info!("Number of cities in hashtable: {}", cities.len());
+
+    let mut sorted_cities: Vec<(String, WeatherSummary)> = cities.into_iter().collect();
+    sorted_cities.sort_by(|a, b| {a.0.cmp(&b.0)});
+    
+    sorted_cities.into_iter().for_each(|(city, stats)| {
+        let mean = stats.sum/stats.reading_count;
+        println!("{}={}/{}/{}", city, stats.min, mean, stats.max);
+    });
+
+    Ok(())
+}
+
+#[derive(Debug, Default)]
+struct WeatherSummary {
+    min: f64,
+    max: f64,
+    sum: f64,
+    reading_count: f64,
+}
